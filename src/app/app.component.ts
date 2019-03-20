31870be144771149cae257548aa40cdc703fd6c3
r.vanderaspoilden <r.vanderspoilden@kiabi.com>
2019-03-20 13:31:24 +0100

    :tada: : init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'restfull-ng';
+export class AppComponent implements OnInit {
+  public users: User[] = [];
+
+  constructor(private userService: UserService) {
+  }
+
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
+    });
+  }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) { }
+
+  public getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(environment.server.users);
+  }
+}
